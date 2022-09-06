e0573ee8c392b85b3b44756f65a73852c4037cf6
Daniel Cifuentes <dacifuentes@outlook.com>
2022-09-05 21:03:54 -0500

    Se inicializa el proyecto

---
+++ src/test/java/co/edu/uis/o30g02consultorioonline/O30g02ConsultorioOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uis.o30g02consultorioonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class O30g02ConsultorioOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
